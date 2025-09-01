70ce98a39a6d4423b1a19e93d6018b1766237869
Daniel Crowder <daniel@Monumental-i.com>
2025-09-01 10:45:39 -0500

    Add WebGPU type definitions and configuration
    - Add @webgpu/types dependency for proper WebGPU API types
    - Include WebGPU lib in TypeScript configuration
    - Add WebGPU types reference to vite-env.d.ts
    - Fix module loading issues with WebGPU interfaces

--- .vite/deps/react-dom.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-KTS7YJSI.js";
-import "./chunk-UERPKZCM.js";
-import "./chunk-7D4SUZUM.js";
-export default require_react_dom();
-//# sourceMappingURL=react-dom.js.map
